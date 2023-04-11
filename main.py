ca1d3b5714c5437eabc4f4421a07d18b8c22f001
nicerg <130170303+nicerg@users.noreply.github.com>
2023-04-12 01:12:41 +0300

    Update main.py
    1-st commit

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ def mane_function(month_number):
   else: 
     raise Exception('Some unexpected Exception !')
     return False
+  
+def test_function(num_list):
+
+  array = []
+
+  if num_list[0] == " ":
+    raise Exception('First character must be a digit !')
+    return False
+  if num_list[len(num_list) - 1] == " ":
+    raise Exception('Last character must be a digit !')
+    return False
