34803854188291e7e7c1284ba3e5b47d482612e5
nicerg <130170303+nicerg@users.noreply.github.com>
2023-04-12 01:13:58 +0300

    Update main.py
    2-nd commit

--- main.py
+++ main.py
@@ -27,3 +27,29 @@ def test_function(num_list):
   if num_list[len(num_list) - 1] == " ":
     raise Exception('Last character must be a digit !')
     return False
+  
+  num_start = 0
+  num_end = 0
+  space_count = 0
+  
+  for i in num_list:
+    if i != " ":
+      num_end += 1
+      space_count = 0
+    else:
+      array.append(num_list[num_start:num_end])
+      num_start = num_end + 1
+      num_end += 1
+      space_count += 1
+      if space_count >= 2:
+        raise Exception('Only one space is allowed between digits !')
+        return False
+
+  array.append(num_list[num_start:num_end])
+    
+  for j in range(len(array)):
+    for k in range(j+1, len(array)):
+      if array[j] == array[k]:
+        return 'The sequence is not unique'
+        
+  return 'The sequence is unique'
